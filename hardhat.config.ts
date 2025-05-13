b83366b9d8c556b075aaea255479ff5648dd9f3c
neil <qgmfky@gmail.com>
2025-05-13 09:43:50 +0800

    contract compilation optimization

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.19',
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true, // 启用中间表示优化
+    },
+  },
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_URL || '',
